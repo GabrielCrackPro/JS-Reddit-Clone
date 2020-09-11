c34097a2bedf4c374fd93ebab16a8bf4add84cb9
GabrielCrackPro <gabrielcrackpro2001@gmail.com>
2020-09-11 23:28:01 +0200

    Profile page added

--- README.md
+++ README.md
@@ -1,19 +1,23 @@
 # JS-Reddit-Clone
+
 This is a reddit clone
 
 ## CHECKLIST
+
 ### BASIC
+
 - [x] Initialize npm project
 - [x] Install all dependencies
-    - express
-    - nodemon (only for dev)
-    - cors
-    - monk
+  - express
+  - nodemon (only for dev)
+  - cors
+  - monk
 - [x] Create the basic folder structure
 - [x] Create all basic files
 - IMPORTANT: PULL APART THE SCRIPTS
 
 ### FRONT-END
+
 - [x] Create UI
 - [x] Create register / login forms
 - [x] Send register / login data to the server
@@ -22,13 +26,18 @@ This is a reddit clone
 - [x] Create post / comment section
 - [x] Add comments to its post
 - [x] Implement Dark mode
+- [x] Add profile page
 
 ### BACK-END
+
 - [x] Recive user data
 - [x] Recieve post data
+- [x] Recieve / send messages
 - [x] Set Up MongoDB connection
 
 ### TO-DO
+
 - Store user data into MongoDB
 - Change dark mode button when is on
 - Fix insert emojis
+- Fix message box
---
+++ client/scripts/profile.js
@@ -0,0 +1,108 @@
+const API_URL = 'http://localhost:3000'
+const profileContainer = document.querySelector('.profile-container')
+//Set random data
+
+const username = document.querySelector('#username')
+const postCount = document.querySelector('#post-count')
+const commentsCount = document.querySelector('#comments-count')
+const awardsCount = document.querySelector('#awards-count')
+
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * max) + min
+}
+function setNumbers() {
+    postCount.textContent = randomNumber(7, 150)
+    commentsCount.textContent = randomNumber(25, 200)
+    awardsCount.textContent = randomNumber(10, 100)
+}
+const possibleUsernames =
+    ['emanvAnt',
+        'ariBlEnz',
+        'IeNInste',
+        'ToNFOrpi',
+        'AnoRIbLE',
+        'AnoRIbLE',
+        'erBENduL',
+        'YNETemOR']
+function getUsername() {
+    const number = randomNumber(0, 8)
+    switch (number) {
+        case 0:
+            username.textContent = possibleUsernames[0]
+            break
+        case 1:
+            username.textContent = possibleUsernames[1]
+            break
+        case 2:
+            username.textContent = possibleUsernames[2]
+            break
+        case 3:
+            username.textContent = possibleUsernames[3]
+            break
+        case 4:
+            username.textContent = possibleUsernames[4]
+            break
+        case 5:
+            username.textContent = possibleUsernames[5]
+            break
+        case 6:
+            username.textContent = possibleUsernames[6]
+            break
+        case 7:
+            username.textContent = possibleUsernames[7]
+            break
+        case 8:
+            username.textContent = possibleUsernames[8]
+            break
+    }
+}
+window.onload = setNumbers()
+window.onload = getUsername()
+//Social buttons
+const messageBtn = document.querySelector('#message-button')
+const followBtn = document.querySelector('#follow-button')
+const reportBtn = document.querySelector('#report-button')
+
+messageBtn.addEventListener('click', () => {
+    const messageBox = document.createElement('div')
+    messageBox.classList.add('message-box')
+    messageBox.innerHTML = `
+    <form id="message-form">
+    <textarea id="message-text" name="message-text" placeholder="Message"></textarea>
+    <br>
+    <button id="message-button">Send</button>
+    </form>
+    `
+    profileContainer.appendChild(messageBox)
+})
+const messageForm = document.querySelector('#message-form')
+messageForm.addEventListener('submit', (event) => {
+    const messageData = new FormData(messageForm)
+    const messageText = messageData.get('message-text')
+
+    const messageDay = new Date().getDay().toLocaleString()
+    const messageMounth = new Date().getMonth().toLocaleString()
+    const messageYear = new Date().getFullYear().toLocaleString()
+
+    const messageDate = messageDay + '/' + messageMounth + '/' + messageYear
+    const messageUser = 'admin'
+
+    const newMessage = {
+        messageText,
+        messageDate,
+        messageUser
+    }
+    if (messageText == '') {
+        console.error('Please type something')
+    } else {
+        fetch(API_URL, {
+            method: "POST",
+            body: newMessage,
+            headers: {
+                "content-type": "application/json"
+            }
+        })
+    }
+    messageForm.reset()
+    event.preventDefault()
+})
